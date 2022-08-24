492d6c7682da3d3fc720ab73c3230544e5d1282c
George-Creekbed <g.barabba@yahoo.com>
2022-08-24 15:09:40 +0100

    Delete SetUnionOperation.py

--- SetUnionOperation.py
+++
@@ -1,3 +0,0 @@
-# Enter your code here. Read input from STDIN. Print output to STDOUT
-n, english, b, french = int(input()), set(input().split()), int(input()), set(input().split())
-print(len(english | french))
